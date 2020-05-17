5d4a1f8e41bc5f0037f95f0248facf36243db797
Charles Naut <cpn585@gmail.com>
2020-05-17 18:54:20 -0400

    Use redis list to store data

--- main.go
+++ main.go
@@ -18,6 +18,11 @@ type CountryData struct {
 	Deaths             int `json:"deaths"`
 }
 
+type DeathData struct {
+	Date   string
+	Deaths string
+}
+
 func main() {
 	resp, err := http.Get("https://corona.lmao.ninja/v2/countries?sort=cases")
 
@@ -53,13 +58,27 @@ func main() {
 		fmt.Println(err)
 	}
 
-	keys, err := client.Keys("2020*").Result()
+	//keys, err := client.Keys("2020*").Result()
+	deathData := &DeathData{
+		Date:   timeNow,
+		Deaths: deaths,
+	}
+
+	bytes, err := json.Marshal(deathData)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	for _, key := range keys {
-		deaths, _ := client.Get(key).Result()
-		fmt.Println(deaths + " - " + key)
+	client.LPush("deaths", bytes)
+
+	deathDatas, err := client.LRange("deaths", 0, 10).Result()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, currDeathData := range deathDatas {
+		var currDeathDataParsed DeathData
+		json.Unmarshal([]byte(currDeathData), &currDeathDataParsed)
+		fmt.Println(string(currDeathDataParsed.Deaths) + " - " + currDeathDataParsed.Date)
 	}
 }
